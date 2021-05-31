caf5b5ecfa19bc481b20d4013b102b5b7af4ca91
shitong xu <tonyshengxue@outlook.com>
2021-05-31 22:20:43 +0100

    shitong_xu: adding trial file on connecting to database

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://localhost:3306/accounts'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "123"
+db = SQLAlchemy(app)
+pymysql.install_as_MySQLdb()
 
+class Account(db.Model):
+    # id = db.Column(db.INT, autoincrement=True, nullable=False)
+    username = db.Column(db.VARCHAR(20), primary_key=True, nullable=False, index=True)
+    password = db.Column(db.VARCHAR(100), nullable=False)
+
+db.create_all()
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    return "hello world"
+
+
+import auth
+app.register_blueprint(auth.bp)
+
 
 if __name__ == '__main__':
     app.run()
---
+++ auth.py
@@ -0,0 +1,56 @@
+import functools
+from flask import (Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from app import Account, db
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/register', methods=['GET', 'POST'])
+def register():
+    if len(request.args) > 0:
+        username = request.args.get('username', 0, type=str)
+        password = request.args.get('password', 0, type=str)
+        error = None
+        if not username:
+            error = 'username is required'
+        elif not password:
+            error = 'password is required'
+        elif not validateKey(username):
+            error = 'already registered'
+        if error is None:
+            new_one = Account(username=username, password=password)
+            db.session.add(new_one)
+            db.session.commit()
+        # flash(error)
+        return jsonify(result="Success")
+    return render_template('account.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if len(request.args) > 0:
+        username = request.args.get('username', 0, type=str)
+        password = request.args.get('password', 0, type=str)
+        error = None
+        record_user = Account.query.filter_by(username=username).first()
+        if record_user is None:
+            error = 'Incorrect username.'
+        elif not record_user.password == password:
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = record_user.username
+            return jsonify(result="Login Succeed")
+        else:
+            return jsonify(result=error)
+        # flash(error)
+
+    return render_template('login.html')
+
+
+def validateKey(username):
+    record = Account.query.filter_by(username=username).first()
+    return record is None
