79dca4dbb11aea8bb65050b20a47afa3eb9faa01
shitong xu <tonyshengxue@outlook.com>
2021-06-07 23:12:49 +0100

    xu-shitong: working prototype of timeline, ready for parallel work

--- flask_blog/main_page.py
+++ flask_blog/main_page.py
@@ -1,11 +1,24 @@
 from flask import (Blueprint, flash)
 from flask.templating import render_template
-
+import json
 bp = Blueprint("main_page", __name__)
 
 @bp.route("/main")
 def main():
-  note = [{"time": 1900, 
-           "title": "some event", 
-           "description": "some description"}]
-  return render_template("main_page.html", note=note)
+  # TODO: note should be retrieved from database, 
+  #         IS_MAIN_PAGE: boolean field, tell js whether it is main page or edit page
+  #         START is the minimum of START variable in nodes, 
+  #         END is maximum of END in nodes
+  #         NODES is a list of turple, each turple are history nodes of the same priority level, 
+  #               the list of turple is sorted in order, from highest priority to lowest priority
+  #               priority level is calculated as the number of parent nodes in the spanning tree
+  #                  e.g. if node A has no parent, it is the root of spanning tree, priority level 0, in the first list
+  #                          node B's parent node is A, then B has priority level 1, in the second list.
+  note = {"is_main_page": True,
+          "start": 100, "end": 150, 
+          "nodes": [[{"start": 100, "end": 120, "title": "event 1", "content": "content of event 1"}, 
+                    {"start": 110, "end": 130, "title": "event 2", "content": "content of event 2"}, 
+                    {"start": 120, "end": 140, "title": "event 3", "content": "content of event 3"}],
+                    [{"start": 100, "end": 150, "title": "event 4", "content": "content of event 4"}]]
+          }
+  return render_template("main_page.html", note=json.dumps(note))
--- flask_blog/static/timeline.js
+++ flask_blog/static/timeline.js
@@ -1,6 +1,11 @@
-let windowWidth = 720, windowHeight = 400;
+const WIN_WIDTH = 720;
+const CANVAS_WIDTH = 700;
+const MAX_WIN_HEIGHT = 600;
+const NODE_HEIGHT = 15; // each node in timeline take up 10px height
+const HOVER_TITLE_SIZE = 15;
+let IS_MAIN_PAGE=true;
 let cnv;
-let note = {}
+let note;
 
 class HNode {
 
@@ -11,11 +16,11 @@ class HNode {
    *   title: title of node, also used as PK for node
    *   parent: the node is detail of what other history node
    */
-  constructor(start, end, title, parent, x, y, width, height) {
+  constructor(start, end, title, content, x, y, width, height) {
     this.start = start;
     this.end = end;
     this.title = title;
-    this.parent = parent;
+    this.content = content;
 
     this.x = x;
     this.y = y;
@@ -33,77 +38,119 @@ class HNode {
 
   /* display, if mouse hovering on it, show title by the side of block */
   display() {
+    // TODO: if text length is smaller than rect length, show title in the middle of rect
     rect(this.x, this.y, this.width, this.height);
     if (this.mouseHovering()) {
-      textSize(20);
-      text(this.title, this.x + this.width, this.y);
+      // TODO: put title in text box with background colour, so that the displayed text is clearer
+      // TODO: 设置text的图层为最高层，防止别的node的矩形框覆盖text内容
+      textSize(HOVER_TITLE_SIZE);
+      // +10 in x to avoid text overlap with mouse arrow
+      text(this.title, mouseX+10, mouseY+HOVER_TITLE_SIZE);
     }
   }
 
   /* if been clicked, display the information in description below */
-  // TODO: if the page is edit page, should re-render page with previous node information instead
   clicked() {
     if (this.mouseHovering()) {
-      // TODO: get data from backend
-      let textContent = "textContent";
-      document.getElementById("description_id").innerHTML
-        = `<h2>${this.title}</h2>\n
-           <p>${textContent}</p>`;
+      if (IS_MAIN_PAGE) {
+        document.getElementById("description_id").innerHTML
+          = `<h2>${this.title}</h2>\n
+            <p>${this.content}</p>`;
+      } else {
+        // TODO: the page is edit page, should re-render page with previous node information instead
+        document.getElementById("description_id").innerHTML
+          = "";
+      }
     }
   }
 
 }
 
-let hnodeList = [], start, end;
+let nodeCollections = []; // collection of nodes displayed in canvas
 
+/* generate a list of integer, represent what level each corrisponding element in LIST should be */
 function nonOverlapGenerator(list) {
-  layers = [{"max": start, "content": []}];
-  for (period in list) {
+  let layers = [];
+  let result = []
+  for (periodIndex in list) {
+    let period = list[periodIndex]
+
     let i = 0;
     while (i < layers.length) {
-      if (layers[i].length == 0 || layers[i]["max"] <= period["start"]) {
-        layers[i]["content"] += period;
-        layers[i]["max"] = period["end"];
+       
+      if (layers[i] <= period["start"]) {
+        layers[i] = period["end"];
+        result.push(i);
         break;
       } 
       i++;
     } 
     if (i == layers.length) {
-      layers += {"max": start, "content": []}
-      layers[i]["content"] += period;
-      layers[i]["max"] = period["end"];
+      layers.push(period["end"]);
+      result.push(i);
     }
   }
-  return layers;
+
+  return result;
 }
 
+/* initialise canvas */
 function setup() {
-  cnv = createCanvas(windowWidth, windowHeight);
+  /** STEP1: deciding metadata of timeline */
+  const NODE_HEIGHT = 10; // each node in timeline take up 10px height
+  note = document.getElementById("canvas").getAttribute('note');
+  note = JSON.parse(note); // note is dictionary containing {"start" "end" "nodes"}
+  note_start = note["start"];
+  note_end = note["end"];
+  IS_MAIN_PAGE = note["is_main_page"];
+
+  let totPeriodSpan = note_end - note_start;
+  
+  let total_height = 0; // record total height of timeline, if greater than MAX_WIN_HEIGHT, stop adding node of higher level
+  let layerBase = 0; // record accumulated height of already processed layers, equal to total_height every end of outer loop
+  /* each element in note["nodes"] is a list of nodes belong to the same layer of event */
+  Array.prototype.forEach.call(note["nodes"], l => {
+    let sublayerAlloc = nonOverlapGenerator(l);
+    let i = 0;
+    Array.prototype.forEach.call(l, node => {
+      let start = node["start"], end = node["end"], layerNum = sublayerAlloc[i];
+      let newNode = new HNode(
+        start, 
+        end, 
+        node["title"], 
+        node["content"],
+        (CANVAS_WIDTH * (start - note_start)) / totPeriodSpan, 
+        layerNum * NODE_HEIGHT + layerBase, 
+        (CANVAS_WIDTH * (end - start)) / totPeriodSpan, 
+        NODE_HEIGHT
+      );
+
+      nodeCollections.push(newNode);
+      total_height += NODE_HEIGHT;
+      i++;
+    });
+    layerBase += total_height;
+  });
+
+  cnv = createCanvas(WIN_WIDTH, MAX_WIN_HEIGHT);
+  cnv.parent("canvas");
   // TODO: center the canvas to top center of page
   // TODO: draw a timeline scale at bottom of canvas
 
-  note = document.getElementById("canvas").getAttribute('note');
-  start = note["start"];
-  end = note["end"];
-
-  let levels = new Array(1);
-  levels[0] = [{"start": 100, "end": 120}, 
-               {"start": 110, "end": 130}, 
-               {"start": 120, "end": 140}]
-  let periodList = nonOverlapGenerator(levels[0]);
-  console.log(periodList);
-  // hnodeList += visualDataGenerator(periodList);
+  Array.prototype.forEach.call(nodeCollections, node => {
+    node.display();
+  });
 }
 
 function draw() {
   background(200, 200, 200);
-  for (node in hnodeList) {
-    node.clicked();
-  }
+  Array.prototype.forEach.call(nodeCollections, node => {
+    node.display();
+  });
 }
 
 function mousePressed() {
-  for (node in hnodeList) {
+  Array.prototype.forEach.call(nodeCollections, node => {
     node.clicked();
-  }
+  });
 }
