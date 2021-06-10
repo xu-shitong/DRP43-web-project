64859263c6e4dc4ce974d51e62889b1807884597
shitong xu <tonyshengxue@outlook.com>
2021-06-10 11:33:51 +0100

    xu-shitong: cancel debug mode

--- flask_blog/app.py
+++ flask_blog/app.py
@@ -16,7 +16,7 @@ def setDatabase(app, test=False):
     app.config['SQLALCHEMY_DATABASE_URI'] \
         = 'mysql://' + DATABASE_ACCOUNT + ':' + DATABASE_PASSWORD + '@' \
           + DATABASE_DOMAIN_NAME + ':3306/' + DATABASE_NAME
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://localhost:3306/accounts'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://zhangziheng:@106.52.101.201:3306/accounts'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = APP_CONFIG_KEY
     # app.config['SECRET_KEY'] = "123"
