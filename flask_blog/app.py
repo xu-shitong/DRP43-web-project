250300f4133645983823637e3575c334860f7269
yc1319 <yc1319@imperial.ac.uk>
2021-06-20 19:07:50 +0800

    zzh cyy xst: Forum style added.

--- flask_blog/app.py
+++ flask_blog/app.py
@@ -4,24 +4,23 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
 
-# DATABASE_ACCOUNT = os.environ["DATABASE_ACCOUNT"]
-# DATABASE_PASSWORD = os.environ["DATABASE_PASSWORD"]
-# DATABASE_DOMAIN_NAME = os.environ["DATABASE_DOMAIN_NAME"]
-# DATABASE_NAME = os.environ["DATABASE_NAME"]
-# APP_CONFIG_KEY = os.environ["APP_CONFIG_KEY"]
+DATABASE_ACCOUNT = os.environ["DATABASE_ACCOUNT"]
+DATABASE_PASSWORD = os.environ["DATABASE_PASSWORD"]
+DATABASE_DOMAIN_NAME = os.environ["DATABASE_DOMAIN_NAME"]
+DATABASE_NAME = os.environ["DATABASE_NAME"]
+APP_CONFIG_KEY = os.environ["APP_CONFIG_KEY"]
 
 app = Flask(__name__)
 
 
 def setDatabase(app, test=False):
-    # app.config['SQLALCHEMY_DATABASE_URI'] \
-    #     = 'mysql://' + DATABASE_ACCOUNT + ':' + DATABASE_PASSWORD + '@' \
-    #       + DATABASE_DOMAIN_NAME + ':3306/' + DATABASE_NAME
+    app.config['SQLALCHEMY_DATABASE_URI'] \
+        = 'mysql://' + DATABASE_ACCOUNT + ':' + DATABASE_PASSWORD + '@' \
+          + DATABASE_DOMAIN_NAME + ':3306/' + DATABASE_NAME
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://zhangziheng:@106.52.101.201:3306/accounts'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://localhost:3306/accounts'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # app.config['SECRET_KEY'] = APP_CONFIG_KEY
-    app.config['SECRET_KEY'] = "123"
+    app.config['SECRET_KEY'] = APP_CONFIG_KEY
+    # app.config['SECRET_KEY'] = "123"
     db = SQLAlchemy(app)
     pymysql.install_as_MySQLdb()
     return db
